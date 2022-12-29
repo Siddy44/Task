47d9614ce0378b8614d0fe0287b375b33c5feff8
Siddy44 <siddharth.kandoria2@gmail.com>
2022-12-29 16:41:01 +0530

    Parametrized Action

---
+++ WebApplication/WebApplication/Controllers/ParaActionController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication.Controllers
+{
+    public class ParaActionController : Controller
+    {
+       //Parametrised Action
+        public ActionResult Def(int id)  //default parameter
+        {
+            return Content("id is "+id);
+        }
+      
+            public ActionResult Index(int year,int ? month)
+            {
+            return Content("year " + year + " optional/month " +month );
+            }
+        
+    }
+}
