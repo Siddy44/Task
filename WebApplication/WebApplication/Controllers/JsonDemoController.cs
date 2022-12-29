e7df1c340d14e1d07f39364f626abcc3409e2cfb
Siddy44 <siddharth.kandoria2@gmail.com>
2022-12-29 15:56:05 +0530

    JsonResult Type

---
+++ WebApplication/WebApplication/Controllers/JsonDemoController.cs
@@ -0,0 +1,40 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication.Models;
+
+namespace WebApplication.Controllers
+{
+    //JsonResult Type
+    public class JsonDemoController : Controller
+    {
+        public JsonResult Index()
+        {
+            JsonData myobj = new JsonData()
+            {
+                id = 1,
+                name = "Sid",
+                email = "sidd@gmail.com"
+            };
+            var data = JsonConvert.SerializeObject(myobj);
+            return Json(data, JsonRequestBehavior.AllowGet);
+        }
+        public JsonResult Folder()
+        {
+            JsonData myobj = new JsonData()
+            {
+                id = 1,
+                name = "Sid",
+                email = "sidd@gmail.com"
+            };
+            string path = "F://MvcJson.txt";
+            string jsonData = JsonConvert.SerializeObject(myobj);
+            System.IO.File.WriteAllText(path, jsonData);
+            return Json(jsonData, JsonRequestBehavior.AllowGet);
+
+        }
+    }
+}
---
+++ WebApplication/WebApplication/Models/JsonData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication.Models
+{
+    public class JsonData
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string email { get; set; }
+    }
+}
