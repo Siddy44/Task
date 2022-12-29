d2d3fd01ca8f137b4d23886bc7b435bbbec3bc7e
Siddy44 <siddharth.kandoria2@gmail.com>
2022-12-29 18:35:35 +0530

    Conventional Routing

--- WebApplication/WebApplication/App_Start/RouteConfig.cs
+++ WebApplication/WebApplication/App_Start/RouteConfig.cs
@@ -12,7 +12,11 @@ namespace WebApplication
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-
+            routes.MapRoute(
+            name: "Default1", //Conventional Routing
+            url: "movie/released/{year}/{month}",
+            defaults: new { controller = "Movie", action = "Index" },
+            new { year = @"\d{4}", month = @"\d{2}" });
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
