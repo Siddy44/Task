67d99295c5abc9d5165f643c3fabbbee0c0ab459
Siddy44 <siddharth.kandoria2@gmail.com>
2022-12-29 17:11:14 +0530

    Adding ViewModel

---
+++ WebApplication/WebApplication/Controllers/MovieDetailController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication.Models;
+using WebApplication.Models.ViewModel;
+
+namespace WebApplication.Controllers
+{
+    public class MovieDetailController : Controller
+    {
+       
+      
+        
+            public ActionResult Popcorn()
+            {
+                var hello = new Movies() { Title = "Mission Impossible!",Rating=7.7 };
+
+              
+
+                var customers = new List<Customer>
+            {
+                 new Customer {Name="Eric" },
+                 new Customer {Name="Hitler" }
+            };
+
+
+
+
+
+
+
+                var obj = new RandomViewModel()
+                {
+                    Movie = hello,
+                    customers = customers,
+
+                };
+
+                return View(obj);
+                
+            }
+        }
+    }
---
+++ WebApplication/WebApplication/Models/ViewModel/RandomViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebApplication.Models;
+
+namespace WebApplication.Models.ViewModel
+{
+    public class RandomViewModel
+    {
+        public Movies Movie { get; set; }
+        public List<Customer> customers { get; set; }
+        public string data { get; set; }
+    }
+}
